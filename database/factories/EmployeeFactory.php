7bf5a0736bbc49a481a2dca8895785a51872f22e
mosama88 <mosama88@hotmail.com>
2024-05-29 18:39:03 +0300

    Employee factory

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\JobGrade;
+use App\Models\Address;
+use App\Models\Department;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employee>
+ */
+class EmployeeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->randomElement(['احمد الأدغم', 'طارق حامد', 'يحي حسن', 'محمد أسامه', 'علاء رجب', 'حسن شوقى']),
+            'phone' => $this->faker->regexify('/^(012|015|010|011)[0-9]{8}$/'),
+            'alter_phone'=>$this->faker->regexify('/^(012|015|010|011)[0-9]{8}$/'),
+            'hiring_date' => $this->faker->dateTimeBetween('2000-05-01', '2016-05-01')->format('Y-m-d'),
+            'start_from'=>$this->faker->dateTimeBetween('2000-05-01', '2016-05-01')->format('Y-m-d'),
+            'job_grades_id'=>JobGrade::all()->random()->id,
+            'address_id'=>Address::all()->random()->id,
+            'department_id'=>Department::all()->random()->id,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             JobGradeseeder::class,
             DepartmentSeeder::class,
+            Employeeseeder::class,
         ]);
 
 
