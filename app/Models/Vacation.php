c872d9083dd4f4987597e1989f87b76a4ae081ed
mosama88 <mosama88@hotmail.com>
2024-06-02 17:13:04 +0300

    vacatione editadddone50055

--- app/Models/Vacation.php
+++ app/Models/Vacation.php
@@ -120,31 +120,4 @@ class Vacation extends Model
 
         return $totalDays;
     }
-
-    // public function calcAllVacation()
-    // {
-    //     // Initialize the total vacation days
-    //     $totalVacationDays = 0;
-    
-    //     // Loop through each vacation associated with the employee
-    //     foreach ($this->vacationEmployee as $vacation) {
-    //         // Check the type of vacation and add the corresponding number of days to the total
-    //         switch ($vacation->type) {
-    //             case 'emergency':
-    //             case 'regular':
-    //             case 'Annual':
-    //             case 'satisfying':
-    //                 $totalVacationDays += $vacation->num_of_days;
-    //                 break;
-    //             // Add cases for other types of vacations if needed
-    //         }
-    //     }
-    
-    //     // Subtract the total vacation days from the total available days
-    //     $remainingDays = $this->num_of_days - ($totalVacationDays + $this->totalDays);
-    
-    //     // Return the remaining days
-    //     return $remainingDays;
-    // }
-
 }
