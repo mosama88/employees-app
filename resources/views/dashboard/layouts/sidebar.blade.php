af3bb9af4cff1aa2c61b18d7a48f7b1bbe9142aa
mosama88 <mosama88@hotmail.com>
2024-06-02 19:35:20 +0300

    done Policy Login2

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -38,26 +38,30 @@
         <ul class="side-menu">
             <li class="side-item side-item-category">برنامج إدارة شئون الموظفين</li>
 
+            @if (auth('admin')->check())
+                <li class="slide">
+                    <a class="side-menu__item" data-toggle="slide" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24">
+                            <path d="M0 0h24v24H0V0z" fill="none" />
+                            <path d="M19 5H5v14h14V5zM9 17H7v-7h2v7zm4 0h-2V7h2v10zm4 0h-2v-4h2v4z" opacity=".3" />
+                            <path
+                                d="M3 5v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2zm2 0h14v14H5V5zm2 5h2v7H7zm4-3h2v10h-2zm4 6h2v4h-2z" />
+                        </svg>
 
-            <li class="slide">
-                <a class="side-menu__item" data-toggle="slide" href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24">
-                        <path d="M0 0h24v24H0V0z" fill="none" />
-                        <path d="M19 5H5v14h14V5zM9 17H7v-7h2v7zm4 0h-2V7h2v10zm4 0h-2v-4h2v4z" opacity=".3" />
-                        <path
-                            d="M3 5v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2zm2 0h14v14H5V5zm2 5h2v7H7zm4-3h2v10h-2zm4 6h2v4h-2z" />
-                    </svg>
-                    <span class="side-menu__label">الضبط العام</span><i class="angle fe fe-chevron-down"></i></a>
-                <ul class="slide-menu">
-                    <li><a class="slide-item" href="{{ route('dashboard.holidays.index') }}">العطلات الرسمية</a></li>
-                    <li><a class="slide-item" href="{{ route('dashboard.jobgrades.index') }}">الدرجات الوظيفيه</a></li>
-                    <li><a class="slide-item" href="{{ route('dashboard.departments.index') }}">النيابات</a></li>
-                    <li><a class="slide-item" href="{{ route('dashboard.vacations.settingVacation') }}">إعدادات
-                            الاجازات</a></li>
-                    <li><a class="slide-item" href="{{ route('dashboard.employees.index') }}">الموظفين</a></li>
-                </ul>
+                        <span class="side-menu__label">الضبط العام</span><i class="angle fe fe-chevron-down"></i></a>
+                    <ul class="slide-menu">
+                        <li><a class="slide-item" href="{{ route('dashboard.holidays.index') }}">العطلات الرسمية</a>
+                        </li>
+                        <li><a class="slide-item" href="{{ route('dashboard.jobgrades.index') }}">الدرجات الوظيفيه</a>
+                        </li>
+                        <li><a class="slide-item" href="{{ route('dashboard.departments.index') }}">النيابات</a></li>
+                        <li><a class="slide-item" href="{{ route('dashboard.vacations.settingVacation') }}">إعدادات
+                                الاجازات</a></li>
+                        <li><a class="slide-item" href="{{ route('dashboard.employees.index') }}">الموظفين</a></li>
+                    </ul>
 
-            </li>
+                </li>
+            @endif
 
 
             <li class="slide">
@@ -76,7 +80,6 @@
                     <li><a class="slide-item" href="{{ route('dashboard.show.vacation') }}">أجازات الموظفين</a></li>
                 </ul>
             </li>
-
 
 
         </ul>
